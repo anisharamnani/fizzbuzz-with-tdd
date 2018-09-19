5a1384cf902a79f55d85bfc4ecc134b81438baaa
Anisha Ramnani <anisha.ramnani@gmail.com>
2018-09-19 16:41:56 -0400

    remove first test case since test coverage is passing

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,6 @@
 require './lib/fizzbuzz.rb'
 
 describe '#fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-   expect(fizzbuzz(3)).to eq 'fizz'
-  end
 
   it 'returns a number when not passed 3' do
     expect(fizzbuzz(1)).to eq(1)

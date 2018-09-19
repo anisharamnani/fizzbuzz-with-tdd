79bd867dc19699831c9c52e6b5195658946e2664
Anisha Ramnani <anisha.ramnani@gmail.com>
2018-09-19 16:46:31 -0400

    return fizz when a number is a multiple of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,11 +6,11 @@ describe '#fizzbuzz' do
     expect(fizzbuzz(1)).to eq(1)
   end
 
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
-  end
-
   it 'returns "fizz when the number is a multiple of 3' do
     expect(fizzbuzz(9)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when the number is a multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
   end
 end

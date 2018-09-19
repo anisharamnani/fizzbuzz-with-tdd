568d366500549bc030c4a40f5d765b7943b770f7
Anisha Ramnani <anisha.ramnani@gmail.com>
2018-09-19 15:54:07 -0400

    consider case where the number is not equal to 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe '#fizzbuzz' do
   it 'returns "fizz" when passed 3' do
    expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns a number when not passed 3' do
+    expect(fizzbuzz(1)).to eq(1)
+  end
 end
